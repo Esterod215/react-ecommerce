aae0ec0d32b19347be29e1a9011bec9448a62902
Esterod215 <estevan.rodriguez215@gmail.com>
2022-09-30 21:12:12 -0700

    set up initial filter context we will use for filtering products.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App'
+import App from './App';
 import { ProductsProvider } from './context/products_context';
+import { FilterProvider } from './context/filter_context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
 	<ProductsProvider>
-		<App />
+		<FilterProvider>
+			<App />
+		</FilterProvider>
 	</ProductsProvider>
-)
+);
---
+++ src/reducers/filter_reducer.js
@@ -0,0 +1,21 @@
+import {
+	LOAD_PRODUCTS,
+	SET_LISTVIEW,
+	SET_GRIDVIEW,
+	UPDATE_SORT,
+	SORT_PRODUCTS,
+	UPDATE_FILTERS,
+	FILTER_PRODUCTS,
+	CLEAR_FILTERS,
+} from '../actions';
+
+const filter_reducer = (state, action) => {
+	switch (action.type) {
+		case LOAD_PRODUCTS:
+			return { ...state, allProducts: action.payload };
+		default:
+			return state;
+	}
+};
+
+export default filter_reducer;
