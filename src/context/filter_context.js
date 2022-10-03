e816eccafb7be6a89b2441829140fd33ebdc685f
Esterod215 <estevan.rodriguez215@gmail.com>
2022-10-02 17:38:13 -0700

    Set up sort component and context to set up functionality of the itemslater.

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -16,6 +16,7 @@ const initialState = {
 	allProducts: [],
 	filteredProducts: [],
 	gridView: true,
+	sort: 'price-lowest',
 };
 
 const FilterContext = React.createContext();
@@ -24,11 +25,24 @@ export const FilterProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState);
 	const { products } = useProductsContext();
 
+	const setGridView = () => {
+		dispatch({ type: SET_GRIDVIEW });
+	};
+	const setListView = () => {
+		dispatch({ type: SET_LISTVIEW });
+	};
+
+	const changeSort = (e) => {
+		dispatch({ type: UPDATE_SORT, payload: e.target.value });
+	};
+
 	useEffect(() => {
 		dispatch({ type: LOAD_PRODUCTS, payload: products });
 	}, [products]);
+
 	return (
-		<FilterContext.Provider value={{ ...state }}>
+		<FilterContext.Provider
+			value={{ ...state, setGridView, setListView, changeSort }}>
 			{children}
 		</FilterContext.Provider>
 	);
