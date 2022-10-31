f8adf7fb29e2cd627a986d9519c5defecfbe8c4b
Estevan <estevan.rodriguez10@yahoo.com>
2022-10-31 14:26:51 -0700

    Added Netlify and Stripe packages and made minor fixes.

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -1,55 +1,59 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-import Loading from './Loading';
-import Error from './Error';
-import Product from './Product';
+import Loading from "./Loading";
+import Error from "./Error";
+import Product from "./Product";
 
-import { useProductsContext } from '../context/products_context';
+import { useProductsContext } from "../context/products_context";
 
 const FeaturedProducts = () => {
-	const { products_loading, products_error, featured_products } =
-		useProductsContext();
+  const { products_loading, products_error, featured_products } =
+    useProductsContext();
 
-	if (products_loading) return <Loading />;
-	if (products_error) return <Error />;
+  if (products_loading) return <Loading />;
+  if (products_error) return <Error />;
 
-	return (
-		<Wrapper className='section'>
-			<div className='title'>
-				<h2>Featured Products</h2>
-				<div className='underline'></div>
-			</div>
-			<div className='section-center featured'>
-				{featured_products.map((product) => {
-					return <Product key={product.id} product={product} />;
-				})}
-			</div>
-		</Wrapper>
-	);
+  return (
+    <Wrapper className="section">
+      <div className="title">
+        <h2>Featured Products</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="section-center featured">
+        {featured_products.map(product => {
+          return <Product key={product.id} product={product} />;
+        })}
+      </div>
+      <Link to="/products" className="btn">
+        All Products
+      </Link>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
-	background: var(--clr-grey-10);
-	.featured {
-		margin: 4rem auto;
-		display: grid;
-		gap: 2.5rem;
-		img {
-			height: 225px;
-		}
-	}
-	.btn {
-		display: block;
-		width: 148px;
-		margin: 0 auto;
-		text-align: center;
-	}
-	@media (min-width: 576px) {
-		.featured {
-			grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
-		}
-	}
+  background: var(--clr-grey-10);
+  .featured {
+    margin: 4rem auto;
+    display: grid;
+    gap: 2.5rem;
+    img {
+      height: 225px;
+    }
+  }
+  .btn {
+    display: block;
+    width: 148px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  @media (min-width: 576px) {
+    .featured {
+      grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
+    }
+  }
 `;
 
 export default FeaturedProducts;
