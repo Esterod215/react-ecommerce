13a5d15b440b4fd0fe82f9dd2b578b94d73dad47
Estevan <estevan.rodriguez10@yahoo.com>
2022-11-02 21:05:44 -0700

    Handled stripe checkout form and you can now successfully finish transactions using test card provided by stripe.

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -63,17 +63,52 @@ const CheckoutForm = () => {
     createPaymentIntent();
   }, []);
 
-  const handleChange = async event => {};
+  const handleChange = async event => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
 
-  const handleSubmit = async ev => {};
+  const handleSubmit = async ev => {
+    ev.preventDefault();
+    setProcessing(true);
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement)
+      }
+    });
+    if (payload.error) {
+      setError(`Payment Failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+      setTimeout(() => {
+        clearCart();
+        navigate("/");
+      }, 5000);
+    }
+  };
 
   return (
     <div>
+      {succeeded ? (
+        <article>
+          <h4>Thank You, Your Payment was Successful!</h4>
+          <h4>Redirecting to Homepage</h4>
+        </article>
+      ) : (
+        <article>
+          <h4>Hello, {myUser && myUser.name}</h4>
+          <p>Your total is {formatPrice(totalAmount + shippingFee)}</p>
+          <p>Test Card: 4242 4242 4242 4242</p>
+        </article>
+      )}
       <form id="payment-form" onSubmit={handleSubmit}>
         <CardElement
           id="card-element"
           options={cardStyle}
-          onChnage={handleChange}
+          onChange={handleChange}
         />
         <button disabled={processing || disabled || succeeded} id="submit">
           <span id="button-text">
